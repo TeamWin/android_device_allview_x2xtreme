5299358ee648f80dc365f736753116783507a444
bemolxd <kalkobb99@gmail.com>
2016-01-28 16:39:11 +0100

    New device tree

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,7 +1,8 @@
+#Starting AndroidBoard.mk
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
-# include the non-open-source counterpart to this file
+-include vendor/allview/x2xtreme/AndroidBoardVendor.mk
--- README.md
+++
@@ -1,2 +0,0 @@
-# android_device_allview_x2xtreme
-Allview X2 Soul Xtreme
